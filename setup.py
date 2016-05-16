3d6e162acec957a1502751c59f39b7dbc82b8816
kims1 <kim_sung_e@cat.com>
2016-05-16 16:33:02 -0500

    Adding REST API Setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+import distutils.core
+
+#TODO: Make sure that the packages are correctly setup
+distutils.core.setup(
+    name='SEKPythonAPI',
+    version='1.0.0',
+    packages=['djangorestframework, markdown, django-filter'],
+    url='',
+    license='GNU General Public License v3.0',
+    author='Sung Eun Kim',
+    author_email='kse0417@hotmail.com',
+    description=''
+)
